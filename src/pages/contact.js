a769ada27fc490e52c85135f3e0b82e621150232
reetesh818 <reet.ug19cse18@opju.ac.in>
2021-11-30 22:33:43 +0530

    dummy contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,57 @@
+import * as React from "react"
+import { Link, graphql, StaticQuery } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import HeroSection from "../components/Reusable/HeroSection"
+import Infoblock from "../components/Reusable/Infoblock"
+import DualInfoBlock from "../components/Reusable/DualInfoBlock"
+
+import Coursecart from "../components/cart/Coursecart"
+
+const IndexPage = ({ data }) => (
+  <Layout>
+    <Seo title="Home" />
+    <HeroSection
+      title="I write Code"
+      subtitle="LearnCodeOnline.in"
+      heroclass="hero-background"
+      image={data.img.childImageSharp.fluid}
+    />
+    <Infoblock heading="About Us" />
+    <Coursecart courses={data.mycourses} />
+    <DualInfoBlock heading="Our Team" />
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "heromain.png" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    mycourses: allContentfulCourses {
+      edges {
+        node {
+          id
+          title
+          price
+          category
+          description {
+            description
+          }
+          image {
+            gatsbyImageData(height: 140, width: 230)
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
